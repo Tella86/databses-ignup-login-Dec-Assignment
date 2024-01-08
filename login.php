d15ddfe8fa33a767d1c083381ed3a63cb30877ea
Tella86 <tellazone0@gmail.com>
2024-01-08 18:33:49 +0300

    first commit

---
+++ login.php
@@ -0,0 +1,106 @@
+<?php
+
+$is_invalid = false;
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    
+    $mysqli = require __DIR__ . "/database.php";
+    
+    $sql = sprintf("SELECT * FROM user
+                    WHERE email = '%s'",
+                   $mysqli->real_escape_string($_POST["email"]));
+    
+    $result = $mysqli->query($sql);
+    
+    $user = $result->fetch_assoc();
+    
+    if ($user) {
+        
+        if (password_verify($_POST["password"], $user["password_hash"])) {
+            
+            session_start();
+            
+            session_regenerate_id();
+            
+            $_SESSION["user_id"] = $user["id"];
+            
+            header("Location:index.php");
+            exit;
+        }
+    }
+    
+    $is_invalid = true;
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+    <link rel="shortcut icon" href="./assets/images/logo.jpeg" type="image/x-icon">
+    
+
+<style type="text/css">
+html,
+body {
+    height: 100%;
+}
+html {
+    display: table;
+    margin: auto;
+}
+body {
+    display: table-cell;
+    vertical-align: middle;
+}
+.margin {
+  margin: 0 !important;
+}
+</style>
+  
+        
+<body>
+
+<center>
+      <img src="./assets/images/logo.jpeg" alt="Eng.Ziroh" width="80">
+     </center>
+<center>
+    <h1>Login</h1>
+    </section>
+    <?php if ($is_invalid): ?>
+        <em>Invalid login</em>
+    <?php endif; ?>
+    
+    <form method="post">
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email"
+               value="<?= htmlspecialchars($_POST["email"] ?? "") ?>">
+        
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password">
+        
+        <div class="button-login">
+        <button>Log in</button>
+        </div>
+        
+    </form>
+  </center>
+    <div>
+     <footer class="page-footer">
+            <div class="footer-copyright">
+              <div class="container">
+                Built by: Eng.Ziroh |The Home Of Developers<br>
+                © copyright 2023 |All Rights Reserved
+              
+              </div>
+</body>
+</html>
+
+
+
+
+
+
+
